2c8b683bb2fdf9007ab5f41822652666f0804f8d
Chefiai <89664585+Chefiai@users.noreply.github.com>
2024-06-15 14:43:11 +0200

    Add files via upload

---
+++ my-new-maven-project/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        printData();
+    }
+
+    public static void printData() {
+        String data = "Example data";
+        System.out.println("Data: " + data);
+    }
+}
